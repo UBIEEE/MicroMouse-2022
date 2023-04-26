6dac2df7e71590c1ccb14439d8cd036293b52f1d
Marcos De La Osa Cruz <marcosde@buffalo.edu>
2023-04-26 15:38:10 -0700

    updated test.c for stderr stuff

--- tests/test.c
+++ tests/test.c
@@ -2,9 +2,10 @@
 #include "../src/algorithm.h"
 #include <stdbool.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 
-void *test1(cell);
+void *test1(cell, FILE *input_file);
 
 //************
 //TESTING FILE
@@ -22,31 +23,40 @@ int top_of_stack = 0;
 //     uint8_t position; // first 4 - x       last 4 - y
 // } cell;
 
-FILE *var = stderr //??the right way to connect to standered error??
 
-cell testCell = { 0b11100001, 0b11111111} //3 boarders and 1 neighbor 
-void *test1(cell testCell){
+cell testCell = { 0b11100001, 0b11111111}; //3 boarders and 1 neighbor 
+void *test1(cell testCell, FILE *input_file){
+    FILE *var = (input_file == NULL) ? stderr : input_file;
+
 if(has_top_border(testCell) == true){ //Has a 1 in the first bit
-    fprintf(var, "True")
+    fprintf(var, "True");
 }else{
-    fprintf(var, "error on top")
+    fprintf(var, "error on top");
 }
 
 if(has_bottom_border(testCell) == true){ //Has a 1 in the second bit
-        fprintf(var, "True")
+        fprintf(var, "True");
 }else{
-    fprintf(var, "error on bottom")
+    fprintf(var, "error on bottom");
 }
 
 if(has_right_border(testCell) == true){//Has a 1 in the third bit
-    fprintf(var, "True")
+    fprintf(var, "True");
 }else{
-    fprintf(var, "error on right")
+    fprintf(var, "error on right");
 }
 
 if(has_left_border(testCell) != true){ //Has a 0 in the fourth bit
-        fprintf(var, "True")
+        fprintf(var, "True");
 }else{
-    fprintf(var, "error on left")
+    fprintf(var, "error on left");
 }
 }//end of method
+
+// Can be thrown in the case we are segfaulting
+void panic(FILE *input_file, char *message){
+    FILE *fptr = (input_file == NULL) ? stderr : input_file;
+    fprintf(fptr, message);
+    exit(1);
+    return;
+}
