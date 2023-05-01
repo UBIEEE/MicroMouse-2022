c6ff3d6fecb6239f88cbb01fbc2315aaee6a77aa
Eric-Butcher <ebutcher66@gmail.com>
2023-05-01 15:32:43 -0400

    Updated testing in runner

--- src/runner.c
+++ src/runner.c
@@ -34,10 +34,10 @@ void testing(){
         }
     }
 
-    cell a = {0b11110000, 0b00000000};
-    cell b = {0b11110000, 0b00000001};
-    cell c = {0b11110000, 0b00000010};
-    cell d = {0b11110000, 0b00000011};
+    cell* a = &grid[0][0];
+    cell* b = &grid[0][1];
+    cell* c = &grid[0][2];
+    cell* d = &grid[0][3];
 
     push(a);
     push(b);
@@ -45,20 +45,20 @@ void testing(){
     push(d);
 
     
-    printf("Info for a: X: %i Y: %i\n", get_x(a), get_y(a));
-    printf("Info for b: X: %i Y: %i\n", get_x(b), get_y(b));
-    printf("Info for c: X: %i Y: %i\n", get_x(c), get_y(c));
-    printf("Info for d: X: %i Y: %i\n", get_x(d), get_y(d));
+    printf("Info for a: X: %i Y: %i\n", get_x(*a), get_y(*a));
+    printf("Info for b: X: %i Y: %i\n", get_x(*b), get_y(*b));
+    printf("Info for c: X: %i Y: %i\n", get_x(*c), get_y(*c));
+    printf("Info for d: X: %i Y: %i\n", get_x(*d), get_y(*d));
 
-    cell result = pop();
-    printf("Info for popped cell 1: X: %i Y: %i\n", get_x(result), get_y(result));
-
-    result = pop();
-    printf("Info for popped cell 2: X: %i Y: %i\n", get_x(result), get_y(result));
+    cell* result = pop();
+    printf("Info for popped cell 1: X: %i Y: %i\n", get_x(*result), get_y(*result));
 
     result = pop();
-    printf("Info for popped cell 3: X: %i Y: %i\n", get_x(result), get_y(result));
+    printf("Info for popped cell 2: X: %i Y: %i\n", get_x(*result), get_y(*result));
 
     result = pop();
-    printf("Info for popped cell 4: X: %i Y: %i\n", get_x(result), get_y(result));
+    printf("Info for popped cell 3: X: %i Y: %i\n", get_x(*result), get_y(*result));
+
+    result = pop();
+    printf("Info for popped cell 4: X: %i Y: %i\n", get_x(*result), get_y(*result));
 }
